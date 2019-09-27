8efd68c38df9c67db2e45b1bc432057183a4dddd
tumiMsaki <245551699@qq.com>
2019-09-27 19:12:25 +0800

    throttle

---
+++ throttle.js
@@ -0,0 +1,29 @@
+function throttle(method, wait, type) {
+  if(type===1){
+      let previous = 0
+  }else if(type===2){
+      let timeout
+  }
+  let throttle = function(...args) {
+    return new Promise(resolve => {
+      let context = this
+      if(type===1){
+          let now = Date.now()
+          if (now - previous > wait) {
+            let result = method.apply(context, args)
+            previous = now
+            resolve(result)
+          }
+      }else if(type===2){
+          if (!timeout) {
+              timeout = setTimeout(() => {
+                  timeout = null
+                  let result = method.apply(context, args)
+                  resolve(result)
+              }, wait)
+          }
+      }
+    })
+  }
+  return throttle
+}
