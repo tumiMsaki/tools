d1bba4ac35f62d36b4dd356a1004cd54e040caeb
tumiMsaki <245551699@qq.com>
2019-10-11 00:08:58 +0800

    new

---
+++ New.js
@@ -0,0 +1,22 @@
+const nNew = function(fn, ...args) {
+  let obj = {}
+  obj.__proto__ = fn.prototype
+  // Object.setPrototypeOf(obj, fn.prototype)
+  let result = fn.apply(obj, args)
+  return result instanceof Object ? result : obj
+}
+
+function K(name, age) {
+  this.name = name
+  this.age = age
+}
+
+K.prototype.say = function() {
+  return `my name is ${this.name}, and I'm ${this.age} years old`
+}
+
+
+const k = nNew(K, 'masaki', 20)
+console.log(k.name)
+console.log(k.age)
+console.log(k.say())
