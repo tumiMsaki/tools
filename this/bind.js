f2be10f17be21f218b34320069d95cff22d85a5b
tumiMsaki <245551699@qq.com>
2019-10-11 17:12:13 +0800

    Realize bind

--- this/bind.js
+++ this/bind.js
@@ -1,3 +1,21 @@
-Function.prototyp.nBind = function() {
-  
+Function.prototype.nBind = function(fn, ...args) {
+  let self = this
+  result = function() {
+    self.apply(this instanceof self ? this : fn, [...args].concat([...args]))
+  }
+  result.prototype = this.prototype
+  return result
 }
+
+var foo = {
+  value: 1
+}
+
+function K(name, age) {
+  console.log(name)
+  console.log(this.value)
+}
+
+var k = K.nBind(foo, 'yk')
+
+var kk = new k(12)
