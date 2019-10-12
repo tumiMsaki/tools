0b323ed9dbea4cd88b6a1d56d9ab24c3cf5a6047
tumiMsaki <245551699@qq.com>
2019-10-12 11:21:48 +0800

    for of

---
+++ forOf.js
@@ -0,0 +1,18 @@
+function forOf(obj, cb) {
+  let iterable, result
+  if (typeof obj[Symbol.iterator] !== "function")
+    throw new TypeError(result + "is not iterable")
+  if (typeof cb !== "function")
+    throw new TypeError("cb must be calleble")
+
+  iterable= obj[Symbol.iterator]()
+  result = iterable.next()
+  while (!result.done) {
+    cb(result.value)
+    result = iterable.next()
+  }
+}
+
+let colors = ["red", "green", "blue"];
+
+forOf(colors, res => console.log(res))
