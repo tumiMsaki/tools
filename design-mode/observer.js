8d2784ff81e739b4fa8eb24515dca47732646694
tumiMsaki <245551699@qq.com>
2019-10-15 19:34:32 +0800

    design mode

---
+++ design-mode/observer.js
@@ -0,0 +1,44 @@
+Object.prototype.observer = {
+  subscribers: {
+    
+  },
+
+  subscribe: function(type, fn) {
+    if (!this.subscribers[type]) {
+      this.subscribers[type] = []
+    }
+    this.subscribers[type].push(fn)
+  },
+
+  unsubscribe: function(type, fn) {
+    this.subscribers[type] = this.subscribers[type].filter(item => {
+      return item !== fn
+    })
+  },
+
+  publish: function(type, ...args) {
+    this.subscribers[type].forEach(fn => {
+      fn(...args)
+    })
+  }
+}
+
+const Tom = {
+  read: function(news) {
+    console.log('read',news)
+  },
+  speak: function(news) {
+    console.log('speak:',news)
+  }
+}
+
+
+const Jack = new Object()
+
+Jack.observer.subscribe('new1', Tom.read)
+Jack.observer.subscribe('new2', Tom.speak)
+
+
+Jack.observer.publish('new2', 'cqupt')
+console.log(Jack.observer.subscribers)
+
